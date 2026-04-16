0f06a55cfd3660062370ba07ea6bdf5bf2437b63
Luqina <cuong.m9325@gmail.com>
2026-04-16 18:45:31 +1000

    swap node for netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
   integrations: [tailwind()],
 });
